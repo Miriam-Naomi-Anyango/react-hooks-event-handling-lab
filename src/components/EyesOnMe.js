5fb9b41f2cd96de0e7866fe1faeca16bea4e5fa1
Miriam Anyango <minaoanya@gmail.com>
2022-07-20 09:50:19 +0300

    commit tree a821b92ce844b375579ef78cd28c0da1afef5206

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,24 @@
 // Code EyesOnMe Component Here
+import react from "react"
+
+
+
+function EyesOnMe(){
+    function focus(){
+        console.log('Good!')
+    }
+    function blur(){
+        console.log('Hey! Eyes on me!')
+        }
+    function attention(events){
+        const events= true? focus() : blur()
+    }
+    
+    return (
+        <button>Eyes on me</button>
+    )
+}   
+       
+    
+    
+export default EyesOnMe
